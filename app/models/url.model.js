f87740cec42db194c49d13875d640cdc0a7d637a
dennismagno <dimagnoph@gmail.com>
2020-09-30 23:26:38 +0800

    Updates

--- app/models/url.model.js
+++ app/models/url.model.js
@@ -6,14 +6,14 @@ const YTUrl = function(video) {
 };
 
 YTUrl.getNextVideo = result => {
-  sql.query("SELECT Id, yturl FROM `videos` ORDER BY watched LIMIT 1", (err, res) => {
+  sql.query("SELECT Id, yturl FROM `videos` ORDER BY datetime, id LIMIT 1", (err, res) => {
     if (err) {
       console.log("error: ", err);
       result(null, err);
       return;
     }
 
-    sql.query('UPDATE `videos` Set watched = watched + 1 WHERE Id = ' + res[0]['Id'] + ' ;');
+    sql.query('UPDATE `videos` Set datetime = NOW() WHERE Id = ' + res[0]['Id'] + ' ;');
     console.log("yturl: ", res);
     result(null, res[0]['yturl']);
   });
